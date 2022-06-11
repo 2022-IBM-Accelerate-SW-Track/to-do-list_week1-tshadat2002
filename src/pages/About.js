e4ebe4012c7bb1995555f722f326a7dfc68a4774
tshadat2002 <tshadat2968@bths.edu>
2022-06-11 14:26:25 -0400

    finished about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import picture from '../assets/pfp.JPG'
+import "./About.css";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div className = "split left">
+          <div className = "centered">
+            <img className = "profile_image" 
+            //image goes here
+            src={picture}
+            alt = "Profile Pic">
+            </img>
+          </div>
+        </div>
+        <div className = "split right">
+          <div className='centered'>
+            <div className='name_title'> Taohid Shadat </div>
+            <div className='brief_description'>
+              <p>Hello! My name is Taohid Shadat and I am a rising junior at CUNY Baruch
+              College. I am currently studying Computer Information Systems and have
+              a strong interest in data science and data analytics.</p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
